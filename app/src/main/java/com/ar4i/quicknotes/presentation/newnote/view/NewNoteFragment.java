97d80be166a077e53a19551d9ee51714eae590b6
Pabls <pabls@yandex.ru>
2019-04-14 00:25:34 +0300

    added background icon

--- app/src/main/java/com/ar4i/quicknotes/presentation/newnote/view/NewNoteFragment.java
+++ app/src/main/java/com/ar4i/quicknotes/presentation/newnote/view/NewNoteFragment.java
@@ -3,6 +3,8 @@ package com.ar4i.quicknotes.presentation.newnote.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.ar4i.quicknotes.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NewNoteFragment extends Fragment {
 
@@ -23,4 +26,19 @@ public class NewNoteFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_new_note, container, false);
     }
 
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        FloatingActionButton fab = getActivity().findViewById(R.id.fab_send);
+//
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(v != null)
+//                    v.setAlpha(0.1f);
+//            }
+//        });
+    }
 }
